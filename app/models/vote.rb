8d90b8788edabb6cf8b7ac06046759c86d6d3bc1
Aaron Riggs <riggs.aaron@gmail.com>
2015-09-14 00:19:26 -0700

    attempting to fix problem w/ voting that happens upon deployment

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic: true
 
-  validates :user_id, uniqueness: {scope: :voteable}
+  validates_uniqueness_of :user_id, scope: [:voteable_type, :voteable_id]
 end
