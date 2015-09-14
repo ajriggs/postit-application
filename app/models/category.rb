4640e5bb3a318305e1b6b4f02dcdafccd22dfa9c
Aaron Riggs <riggs.aaron@gmail.com>
2015-09-14 00:32:25 -0700

    adding validation for unique category names

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :post_categories, foreign_key: :category_id
   has_many :posts, through: :post_categories
-  
-  validates :name, presence: true, length: {minimum: 3}
+
+  validates :name, presence: true, length: {minimum: 3}, uniqueness: true
 end
