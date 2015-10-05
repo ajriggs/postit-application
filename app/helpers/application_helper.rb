4dc74e186013094e5e67452dbad3cd6a88d7a23c
Aaron Riggs <riggs.aaron@gmail.com>
2015-10-04 20:32:37 -0700

    removed some junk from ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,9 +13,5 @@ module ApplicationHelper
     end
     datetime.strftime("%D %r %Z")
   end
-
-  # needs tinkering, but I do feel like this logic could be put into a helper, maybe?
-  # def header(asset)
-  #   link_to('All Posts', posts_path) + " &raquo;  #{asset.name}".html_safe
-  # end
+  
 end
